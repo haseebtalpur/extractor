4d44fe3f9527489c7dcba00016b04470ea359802
haseebtalpur <Haseebtalpur2004@gmail.com>
2025-08-30 23:52:19 +0500

    Update settings.py

--- Extractor/settings.py
+++ Extractor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-mf8$+b7y$_jif%(q+6!f%!4fo(2$6c*a!($k_a88u#7^n-hta@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
+
 
 
 # Application definition
